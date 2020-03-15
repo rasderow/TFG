3990b3ea05f76071c9a8a75d3ffa5f13fa6de589
rasderow <acobot@uoc.edu>
2020-03-15 20:47:54 +0100

    Zombie AI

--- Game/Assets/SaraMovement.cs
+++ Game/Assets/SaraMovement.cs
@@ -14,7 +14,7 @@ public class SaraMovement : MonoBehaviour
 
     void Start() {
         rb = GetComponent<Rigidbody2D>();
-        //anim = GetComponent<Animator>();
+        
     }
 
     // Update is called once per frame
@@ -36,22 +36,16 @@ public class SaraMovement : MonoBehaviour
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
 
         // Rotation
-        if (x!=0 || y!=0) {
-            //transform.Rotate(0,0,90f);
+        if (x!=0 || y!=0) {            
             rb.rotation = RotateCharacter(x, y);            
-        }
-
-        
-
-
+        }           
     }
 
     float RotateCharacter(float x, float y) {
         if (x == 1 && y == 1) return 180f;
         if (x == 0 && y == 1) return 225f;
         if (x == -1 && y == 1) return 270f;
-        if (x == -1 && y == 0) return 315f;
-        //if (x == -1 && y == -1) return 360f;
+        if (x == -1 && y == 0) return 315f;        
         if (x == 0 && y == -1) return 45f;
         if (x == 1 && y == -1) return 90f;
         if (x == 1 && y == 0) return 135f;        
---
+++ Game/Assets/ZombieController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieController : MonoBehaviour
+{
+    public Transform target;
+    private Rigidbody2D rb;
+    private float initialRotation = 90f;
+    public float visionDistance;
+    public float attackDistance;
+    public float moveSpeed;
+    public Animator animator;
+
+    // Start is called before the first frame update
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        Vector2 direction = target.position - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        float targetDistance = Vector3.Distance(target.position, transform.position);
+        Debug.Log("targetDistance: " + targetDistance);
+
+        Vector2 actualPosition = rb.transform.position;
+        float actualRotation = rb.rotation;
+
+        Debug.Log("actualPosition: " + actualPosition);
+        Debug.Log("actualRotation: " + actualRotation);
+
+        animator.SetBool("Walking", false);
+        if (targetDistance < visionDistance) {
+            rb.rotation = initialRotation + angle;
+            if (attackDistance < targetDistance) {
+                animator.SetBool("Walking", true);
+                rb.MovePosition(rb.position + direction * moveSpeed * Time.fixedDeltaTime);
+            }
+        }
+
+        else {
+            rb.rotation = actualRotation;
+            rb.MovePosition(actualPosition);
+        }
+
+        /*if (attackDistance < targetDistance && targetDistance <= visionDistance) {
+            animator.SetBool("Walking", true);
+            rb.rotation = initialRotation + angle;
+            rb.MovePosition(rb.position + direction * moveSpeed * Time.fixedDeltaTime);
+                        
+        }
+
+        else {
+            animator.SetBool("Walking", false);
+            if ()
+            rb.rotation = initialRotation + angle;
+        }*/
+        
+
+
+
+
+
+        
+
+        /*if 
+
+        
+        /*Debug.Log("direction: " + direction);
+        Debug.Log("angle: " + angle);*/
+    }
+}
